4c491300c6aa4e50c77c408996445d4d70680cbd
sangharsha ranpise <sangharsharanpise408@gmail.com>
2020-03-05 10:43:31 +0530

    async parallel with object and array

--- ES6 -Programs/asyncParallel.js
+++ ES6 -Programs/asyncParallel.js
@@ -31,3 +31,50 @@ console.log("Result :",result);
 //   getUserAge: '25',
 //   getUserGender: 'female'
 // }
+
+
+async.parallel([
+  function(callback) {
+    setTimeout(function() {
+      console.log('Task One');
+      callback(null, 1);
+    }, 200);
+  },
+  function(callback) {
+    setTimeout(function() {
+      console.log('Task Two');
+      callback(null, 2);
+    }, 100);
+  }
+],
+function(err, results) {
+  console.log(results);
+  // the results array will equal [1, 2] even though
+  // the second function had a shorter timeout.
+  // Task Two
+  // Task One
+  // [ 1, 2 ]
+});
+
+
+// an example using an object instead of an array
+async.parallel({
+  task1: function(callback) {
+    setTimeout(function() {
+      console.log('Task One');
+      callback(null, 1);
+    }, 200);
+  },
+  task2: function(callback) {
+    setTimeout(function() {
+      console.log('Task Two');
+      callback(null, 2);
+    }, 100);
+    }
+}, function(err, results) {
+  console.log(results);
+  // results now equals to:
+  // Task Two
+  // Task One
+  // { task2: 2, task1: 1 }
+});
