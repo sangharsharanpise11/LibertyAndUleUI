f99bae2a4aa0c578a069781b521cfc32591f433c
sangharsha ranpise <sangharsharanpise408@gmail.com>
2020-03-05 17:31:37 +0530

    merge array done

---
+++ ES6 -Programs/MergeArray.js
@@ -0,0 +1,12 @@
+const fruite1 = ['apple', 'banana', 'lemon'];
+const fruite2 = ['melon', 'strawbery', 'guava'];
+
+//conacat
+const combinedArray1 = fruite1.concat(fruite2);
+console.log(combinedArray1);
+
+
+//spread syntax
+
+const combinedArray2 =[...fruite1,...fruite2];
+console.log(combinedArray2);
